fcd20321a9c594b0320271c98cb76eec06a3529c
darkmac78 <36972321+darkmac78@users.noreply.github.com>
2020-05-10 17:12:06 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 mkdir /tmp/oss15.2_MailServer; cd /tmp/oss15.2_MailServer; wget https://raw.githubusercontent.com/darkmac78/darkmac/master/Dockerfile
 clear; echo "Building the openSUSE15.2 image..."; sleep 3
-docker build -t=oss152_mailserver ./; docker run -d --name=oss15_MailServer -v /sys/fs/cgroup:/sys/fs/cgroup:ro oss152_mailserver
+docker build -t=oss152_mailserver ./; 
+clear
+echo "Docker Images:"
+docker images
+docker run -d --name=oss15_MailServer -v /sys/fs/cgroup:/sys/fs/cgroup:ro oss152_mailserver
+echo "Running containers:"
+docker ps -a
 rm -rf /tmp/oss15.2_MailServer
-clear; docker images; docker ps -a
