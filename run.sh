729d4efb49cac455700420040507fd39d7e440c0
darkmac78 <36972321+darkmac78@users.noreply.github.com>
2020-05-10 14:21:28 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+mkdir /tmp/oss15.2_Mailserver
+cd /tmp/oss15.2_MailServer
+wget https://github.com/darkmac78/darkmac/blob/master/Dockerfile -O /tmp/oss15.2_MailServer/Dockerfile
+docker build -t=oss152_mailserver ./
+docker images
+docker ps -a
