fe4111f7f6d5813f1f1f586867c2a657ff8a8a94
darkmac78 <36972321+darkmac78@users.noreply.github.com>
2020-05-10 14:26:46 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 mkdir /tmp/oss15.2_Mailserver
 cd /tmp/oss15.2_MailServer
 wget https://github.com/darkmac78/darkmac/blob/master/Dockerfile -O /tmp/oss15.2_MailServer/Dockerfile
