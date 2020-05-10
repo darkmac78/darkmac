f9a4f1bbbb976437c91022b06f56ada761ee8bd1
darkmac78 <36972321+darkmac78@users.noreply.github.com>
2020-05-10 14:30:39 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-mkdir /tmp/oss15.2_Mailserver
+mkdir /tmp/oss15.2_MailServer
 cd /tmp/oss15.2_MailServer
 wget https://raw.githubusercontent.com/darkmac78/darkmac/master/Dockerfile -O /tmp/oss15.2_MailServer/Dockerfile
 docker build -t=oss152_mailserver ./
 docker images
 docker ps -a
+rm -rf /tmp/oss15.2_MailServer
