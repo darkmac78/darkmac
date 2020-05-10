7aea66efe683d59369a33b54156acd903127d455
darkmac78 <36972321+darkmac78@users.noreply.github.com>
2020-05-10 17:13:58 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-mkdir /tmp/oss15.2_MailServer; cd /tmp/oss15.2_MailServer; wget https://raw.githubusercontent.com/darkmac78/darkmac/master/Dockerfile
-clear; echo "Building the openSUSE15.2 image..."; sleep 3
+mkdir /tmp/oss15.2_MailServer
+cd /tmp/oss15.2_MailServer
+wget https://raw.githubusercontent.com/darkmac78/darkmac/master/Dockerfile
 docker build -t=oss152_mailserver ./; 
 clear
 echo "Docker Images:"
