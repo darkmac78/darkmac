90e6e3a5c55e205a6fa8a88904993cd7fe90d7b6
darkmac78 <36972321+darkmac78@users.noreply.github.com>
2020-05-10 16:26:24 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 mkdir /tmp/oss15.2_MailServer; cd /tmp/oss15.2_MailServer; wget https://raw.githubusercontent.com/darkmac78/darkmac/master/Dockerfile
 clear; echo "Building the openSUSE15.2 image..."; sleep 3
-docker build -t=oss152_mailserver ./
-docker images
-clear
-docker run -d --name=oss15_MailServer -v /sys/fs/cgroup:/sys/fs/cgroup:ro oss152_mailserver
-clear
-echo "Checking if the images were create (you should have 2 images: opensuse/leap:15.2 and oss152_mailserver"
-docker images
-sleep 5
-clear
-echo "Checking if the container is running..."
-docker ps -a
-sleep 5
+docker build -t=oss152_mailserver ./; docker run -d --name=oss15_MailServer -v /sys/fs/cgroup:/sys/fs/cgroup:ro oss152_mailserver
 rm -rf /tmp/oss15.2_MailServer
+clear; docker images; docker ps -a
